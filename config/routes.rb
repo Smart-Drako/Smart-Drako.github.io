8ab216e4450ed3f238ff87156c498bca8b465132
Manuel Robles <manuel.robles@docdigitales.com>
2020-04-18 10:50:01 -0700

    Actualiza login y registro con devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  devise_scope :user do
+    get "/login" => "devise/sessions#new" # custom path to login/sign_in
+    get "/registro" => "devise/registrations#new" # custom path to sign_up/registration
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
