c15f3d596a4708c2d56e9c97ae2403ef865171ed
Manuel Robles <manuel.robles@docdigitales.com>
2020-04-22 17:28:47 -0700

    Ajustes diseño temporales

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @categorias = {
+      "cat-1" => "Asadero",
+      "cat-2" => "Barbería",
+      "cat-3" => "Abarrotes",
+      "cat-4" => "Pizzas",
+      "cat-5" => "Sushi",
+      "cat-6" => "Ferretería",
+      "cat-7" => "Postres",
+      "cat-8" => "Botanas",
+    }
   end
 end
