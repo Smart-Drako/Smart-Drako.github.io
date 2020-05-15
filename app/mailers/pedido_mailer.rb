92ad971ab9198e7109a0afd451e415a0cca2621e
Manuel Robles <manuel.robles@docdigitales.com>
2020-05-15 00:13:53 -0700

    Ajustes pedido mailer

--- app/mailers/pedido_mailer.rb
+++ app/mailers/pedido_mailer.rb
@@ -3,9 +3,9 @@ class PedidoMailer < ApplicationMailer
     @empresa = "Abarrotes La Unica"
     #Variables
     @destinatario_nombre = "Manuel Robles"
-    @destinatario_mensaje = "Este el pedido que realizaste a: #{@empresa}"
+    @destinatario_mensaje = "Realizaste un pedido a #{@empresa}"
     @pedido_numero = "1234"
-    @pedido_fecha = "14/05/20"
+    @pedido_fecha = "14/05/2020"
     @pedido_total = "$345.00"
     @info_cliente = "Informacion del cliente (nombre, dir, telefono ,etc."
     @info_proveedor = "Informacion del proveedor (nombre, dir, telefono ,etc."
