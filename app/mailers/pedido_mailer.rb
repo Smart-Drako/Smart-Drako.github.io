892fe86c6ccfdf99e42f7dea50316dcf2dd7cc3c
Manuel Robles <manuel.robles@docdigitales.com>
2020-05-14 23:44:58 -0700

    Agrega pedido mailer

---
+++ app/mailers/pedido_mailer.rb
@@ -0,0 +1,15 @@
+class PedidoMailer < ApplicationMailer
+  def pedido(destinatario, asunto)
+    @empresa = "Abarrotes La Unica"
+    #Variables
+    @destinatario_nombre = "Manuel Robles"
+    @destinatario_mensaje = "Este el pedido que realizaste a: #{@empresa}"
+    @pedido_numero = "1234"
+    @pedido_fecha = "14/05/20"
+    @pedido_total = "$345.00"
+    @info_cliente = "Informacion del cliente (nombre, dir, telefono ,etc."
+    @info_proveedor = "Informacion del proveedor (nombre, dir, telefono ,etc."
+
+    mail(from: I18n.transliterate("#{@empresa} <pideloencasamx@gmail.com>"), to: I18n.transliterate(destinatario), subject: asunto,  reply_to: '<pideloencasamx@gmail.com>')
+  end
+end
