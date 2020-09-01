2aeba1457308c73b1f01601e72f0358ab6e73f73
Manuel Robles <manuel.robles@docdigitales.com>
2020-08-31 21:36:25 -0700

    Cambia texto confirmacion

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 # server-based syntax
-set :branch, 'staging'
+set :branch, 'staging_imagenes'
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
