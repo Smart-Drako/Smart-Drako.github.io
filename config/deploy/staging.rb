3fc8538373732b1c83c01f87979989ced22331ed
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-11-13 09:03:25 -0800

    branch change

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 # server-based syntax
-set :branch, 'feature/reparto'
+set :branch, 'hotfix/spooky-bugs'
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
