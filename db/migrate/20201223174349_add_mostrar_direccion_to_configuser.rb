af9a0d8ca5edd2bf86991c3c7bfa82a79c36da53
Manuel Robles <manrobless@gmail.com>
2020-12-23 10:06:41 -0800

    mostrar direccion default to true

--- db/migrate/20201223174349_add_mostrar_direccion_to_configuser.rb
+++ db/migrate/20201223174349_add_mostrar_direccion_to_configuser.rb
@@ -1,5 +1,5 @@
 class AddMostrarDireccionToConfiguser < ActiveRecord::Migration[5.2]
   def change
-    add_column :config_users, :mostrar_direccion, :boolean, after: :reparto_activo, default: false
+    add_column :config_users, :mostrar_direccion, :boolean, after: :reparto_activo, default: true
   end
 end
